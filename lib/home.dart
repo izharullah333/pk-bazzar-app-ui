efcae36a8f3bcfe634ff992a3565eed563bcd905
Administrator <khanixhar4@gmail.com>
2023-10-15 00:57:05 +0500

    complete the pak bazaar app ui design

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:pk_bazar_app/custom_container.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
